efdf587f64a876cb56bd215ff705e2737ae0f65b
Sumanth <sumanthreddystar@gmail.com>
2022-08-23 23:31:27 +0530

    batchimage added few files.

--- batchimage/batch_image_cli.py
+++ batchimage/batch_image_cli.py
@@ -1,5 +1,103 @@
+# Package Imports
+import typer
 
+# Module Imports
 from batch_image_module import convert_images
-import cv2
+from batch_image_module import print_files
 
-print(convert_images())
+
+# Typer App
+app = typer.Typer()
+
+
+@app.callback(
+    invoke_without_command=True,
+)
+def main(
+        default: str  = typer.Option(
+            "",
+            "--dict",
+            "-d",
+            help="Python Dictionary / Ruby Hash name",
+        ),
+    ):
+
+    """
+        Process large batches of images in parallel.
+
+        You can,
+
+                🔹 Compress Images - 🗜️
+
+                🔹 Convert Images  - 🔄
+
+                🔹 Crop Images  - 🔲
+
+                🔹 Apply Filters  - 💅
+
+                🔹 Convert Images  - 🔄
+
+
+        \n
+
+        batchimage on Pypi           :  https://pypi.org/project/batchimage
+
+        batchimage on Github         :  https://github.com/insumanth/batchimage
+
+        Please report any issues     :  https://github.com/insumanth/batchimage/issues
+    """
+
+    print(f"Main {default}")
+
+
+
+
+@app.command()
+def display(
+        directory_path: str,
+    ):
+    print(directory_path)
+    print(print_files(directory_path))
+
+
+@app.command()
+def compress(
+
+        source: str = typer.Argument(
+            ...,
+            help="Source Directory containing Image files",
+            metavar="source",
+            # rich_help_panel="compress",
+        ),
+
+        destination: str = typer.Argument(
+            ...,
+            help="Destination Directory where processed images are stored",
+            metavar="destination",
+            # rich_help_panel="compress",
+        ),
+
+
+    ):
+    print(f"{source} \n {destination}")
+
+
+@app.command()
+def convert(
+
+    ):
+    print(f"{item} ")
+
+
+
+
+
+def start() -> None:
+    app()
+
+
+if __name__ == "__main__":
+    app()
+
+
+
--- batchimage/batch_image_module.py
+++ batchimage/batch_image_module.py
@@ -1,7 +1,20 @@
 
 import cv2
+import glob
 
 
 def convert_images():
-
     return "Images Converted"
+
+
+
+def print_files(path:str , is_recersive:bool = False):
+    files = glob.glob(f"{path}/*")
+    return files
+
+
+
+
+
+
+
